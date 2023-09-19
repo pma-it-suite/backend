d9133a170a39e9898ee8555eb515ff53116bbdf0
Ubuntu <adminuser@its-kdns.gs23fk5bqcduhkb31uukumwgkc.bx.internal.cloudapp.net>
2023-09-19 05:27:11 +0000

    added env

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from routes.commands_routes import commands_routes
 from flask_cors import CORS
 from config.main import SERVER_HOST, SERVER_PORT
 import logging
+import argparse
 
 logging.basicConfig(filename='logs.txt',
                     level=logging.INFO,
@@ -30,6 +31,20 @@ app.register_blueprint(commands_routes, url_prefix='/commands')
 def test_endpoint():
     return jsonify({"message": "Hello, FELIPE!"})
 
-
 if __name__ == '__main__':
-    app.run(debug=True, host=SERVER_HOST, port=SERVER_PORT)
+    parser = argparse.ArgumentParser(description='Run the app in debug or production mode.')
+    parser.add_argument('--debug', type=bool, default=False, help='Run in debug mode.')
+    args = parser.parse_args()
+
+    SERVER_HOST = 'localhost' if args.debug else '0.0.0.0'
+    SERVER_PORT = 8080 if args.debug else '8080'
+    
+    if args.debug:
+        app.run(debug=True, host=SERVER_HOST, port=SERVER_PORT)
+    else:
+        file_base = "/var/lib/caddy/.local/share/caddy/certificates/acme-v02.api.letsencrypt.org-directory/its.kdns.ooo/its.kdns.ooo"
+        cert = file_base + ".crt"
+        key = file_base + ".key"
+        context = (cert, key)
+        app.run(debug=False, host=SERVER_HOST, port=SERVER_PORT, ssl_context=context)
+
