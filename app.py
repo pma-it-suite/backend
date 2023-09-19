d2559bf7d37a5e6fb9c25051fce7aa8002491b67
astherath <farceriv@gmail.com>
2023-09-19 02:38:57 -0400

    added device registry

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from routes.user_routes import user_routes
 from routes.subscription_routes import subscription_routes
 from routes.commands_routes import commands_routes
+from routes.device_routes import devices_routes
 from flask_cors import CORS
 from config.main import SERVER_HOST, SERVER_PORT
 import logging
@@ -24,6 +25,7 @@ CORS(app,
 app.register_blueprint(user_routes, url_prefix='/users')
 app.register_blueprint(subscription_routes, url_prefix='/subscriptions')
 app.register_blueprint(commands_routes, url_prefix='/commands')
+app.register_blueprint(devices_routes, url_prefix='/devices')
 
 
 # Test endpoint at root URL
@@ -31,14 +33,19 @@ app.register_blueprint(commands_routes, url_prefix='/commands')
 def test_endpoint():
     return jsonify({"message": "Hello, FELIPE!"})
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Run the app in debug or production mode.')
-    parser.add_argument('--debug', type=bool, default=False, help='Run in debug mode.')
+    parser = argparse.ArgumentParser(
+        description='Run the app in debug or production mode.')
+    parser.add_argument('--debug',
+                        type=bool,
+                        default=False,
+                        help='Run in debug mode.')
     args = parser.parse_args()
 
     SERVER_HOST = 'localhost' if args.debug else '0.0.0.0'
     SERVER_PORT = 8080 if args.debug else '8080'
-    
+
     if args.debug:
         app.run(debug=True, host=SERVER_HOST, port=SERVER_PORT)
     else:
@@ -46,5 +53,7 @@ if __name__ == '__main__':
         cert = file_base + ".crt"
         key = file_base + ".key"
         context = (cert, key)
-        app.run(debug=False, host=SERVER_HOST, port=SERVER_PORT, ssl_context=context)
-
+        app.run(debug=False,
+                host=SERVER_HOST,
+                port=SERVER_PORT,
+                ssl_context=context)
---
+++ routes/device_routes.py
@@ -0,0 +1,67 @@
+from flask import Blueprint, request, jsonify
+from flask_cors import cross_origin
+from config.db import get_database
+from bson.objectid import ObjectId
+import random
+import string
+
+devices_routes = Blueprint('device_routes', __name__, url_prefix='/devices')
+
+# Initialize MongoDB client
+client = get_database()
+db = client["pma-it-suite"]
+users_collection = db["members"]
+devices_collection = db["devices"]
+
+
+@devices_routes.route('/register', methods=['POST'])
+@cross_origin()
+def register_device():
+    data = request.json
+
+    required_fields = ["user_id", "user_secret"]
+    for field in required_fields:
+        if field not in data:
+            return jsonify({
+                "status": "error",
+                "message": f"{field} is required"
+            }), 400
+
+    try:
+        user_id = data["user_id"]
+        filter = {"_id": user_id}
+        print(f"looking for user {user_id} to register device...")
+        user = users_collection.find_one(filter)
+        if not user:
+            return jsonify({"message": f"User with {user_id} not found"}), 404
+
+        if data["user_secret"] != get_user_secret(user_id):
+            return jsonify({"message": f"User secret invalid"}), 401
+
+        device_id = str(ObjectId())
+
+        try:
+            device_id = user["device_ids"] + device_id
+        except:
+            print(f"adding device id ({device_id}) to user ({user_id})...")
+            device_ids = [device_id]
+
+        users_collection.update_one(filter,
+                                    {"$set": {
+                                        "device_ids": device_ids
+                                    }})
+
+        device = {"_id": device_id, "user_id": user_id}
+        devices_collection.insert_one(device)
+
+        return jsonify({
+            "message":
+            f"User {user['username']} updated with device {device_id} successfully"
+        }), 200
+    except Exception as e:
+        return jsonify({"message": f"An error occurred: {str(e)}"}), 500
+
+
+def get_user_secret(user_id: str):
+    # TODO @felipearce
+    return user_id + "-test-token"
